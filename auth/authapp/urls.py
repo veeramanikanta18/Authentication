66880c97d1b84f6963afafdf6b56f6e102ded975
Guntu veera manikanta <manikantadev2@gmail.com>
2023-08-09 22:38:01 +0530

    update urls and views

---
+++ auth/authapp/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path('', views.homepage, name=""),
+    path('register', views.register, name="register"),
+    path('my_login', views.my_login, name="my_login"),
+    path('dashboard', views.dashboard, name="dashboard"),
+]
--- auth/authapp/views.py
+++ auth/authapp/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+def homepage(request):
+    pass
+
+def register(request):
+    pass
+
+def my_login(request):
+    pass
+
+def dashboard(request):
+    pass
