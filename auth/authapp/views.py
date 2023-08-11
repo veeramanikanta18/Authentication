586534a2f03519143cd32695be31491c52ad40be
Guntu veera manikanta <manikantadev2@gmail.com>
2023-08-11 16:50:45 +0530

    update templates

--- auth/authapp/views.py
+++ auth/authapp/views.py
@@ -1,14 +1,60 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-# Create your views here.
+from django.shortcuts import render, redirect
+
+from . forms import CreateUserForm, LoginForm
+
+# Authentication models and functions
+
+from django.contrib.auth.models import auth
+from django.contrib.auth import authenticate, login, logout
+
+
+# This is for homepage:
+
 def homepage(request):
     return render(request, 'authapp/index.html')
 
+
+# This is for register:
+
 def register(request):
-    return render(request, 'authapp/register.html')
+    
+    form = CreateUserForm()
+    if request.method == "POST":
+        form = CreateUserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("my_login")
+    
+    context = {'registerform':form}
+    
+    return render(request, 'authapp/register.html', context=context)
+
+
+
+# This is for login:
 
 def my_login(request):
-    return render(request, 'authapp/my-login.html')
+    
+    form = LoginForm()
+    if request.method == "POST":
+        form = LoginForm(request, data=request.POST)
+        
+        if form.is_valid():
+            username = request.POST.get('username')
+            password = request.POST.get('password')
+            
+            user = authenticate(request, username=username, password=password)
+            
+            if user is not None:
+                auth.login(request, user)
+                return redirect("dashboard")
+       
+    context = {'loginform':form}
+            
+    return render(request, 'authapp/my-login.html', context=context)
+
+
+# This is for dashboard
 
 def dashboard(request):
     return render(request, 'authapp/dashboard.html')
