b07380b5a08d4dff56d14acaa2233c55002ed0e6
Guntu veera manikanta <manikantadev2@gmail.com>
2023-08-11 12:05:34 +0530

    adding templates

--- auth/authapp/views.py
+++ auth/authapp/views.py
@@ -2,13 +2,13 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def homepage(request):
-    pass
+    return render(request, 'authapp/index.html')
 
 def register(request):
-    pass
+    return render(request, 'authapp/register.html')
 
 def my_login(request):
-    pass
+    return render(request, 'authapp/my-login.html')
 
 def dashboard(request):
-    pass
+    return render(request, 'authapp/dashboard.html')
